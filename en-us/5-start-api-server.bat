9dab78b222bcc6f825bda70915a4131e9f33a5f8
YAN Wenkun <code@yanwk.fun>
2025-05-02 21:19:13 +0800

    API mode enable tex by default
    fix #50

--- en-us/5-start-api-server.bat
+++ en-us/5-start-api-server.bat
@@ -20,7 +20,7 @@ set HF_HUB_CACHE=%~dp0\HuggingFaceHub
 @REM ===========================================================================
 
 cd Hunyuan3D-2
-..\python_standalone\python.exe -s api_server.py --host 0.0.0.0 --port 8081
+..\python_standalone\python.exe -s api_server.py --host 0.0.0.0 --port 8081 --enable_tex
 
 cd ..
 
